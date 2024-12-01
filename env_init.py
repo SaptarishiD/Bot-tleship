408b4c1c196754574de48d3e2948c39490146fb4
SaptarishiD <saptarishi.dhanuka@gmail.com>
2024-12-02 01:25:58 +0530

    refactor

--- env_init.py
+++
@@ -1,9 +0,0 @@
-import gymnasium as gym
-from gymnasium.envs.registration import register
-from ppo_env import BattleshipEnv
-
-register(
-    id="BattleshipEnvSD-v0",
-    entry_point="BattleshipEnv",  # Replace with the actual module path
-    kwargs={"board_height": 10, "board_width" : 10, "ships": [2, 3, 3, 4, 5]},
-)
